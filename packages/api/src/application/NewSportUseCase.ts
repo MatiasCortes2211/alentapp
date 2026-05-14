0fcf508f13db72278b0bd2dd489bf602ac41bb6c
MatiasCortes2211 <mcortes@alu.frlp.utn.edu.ar>
2026-05-14 10:45:18 -0300

    feat(sport): agrega mensajes de error a NewSportUseCase integrando zod

--- packages/api/src/application/NewSportUseCase.ts
+++ packages/api/src/application/NewSportUseCase.ts
@@ -1,11 +1,11 @@
-import { z } from "zod";
+import { z, ZodError } from "zod";
 import { SportRepository } from "../domain/SportRepository.js";
 import {Sport, CreateSport } from "@alentapp/shared";
 
 const CreateSportSchema = z.object({
     name: z.string().min(1),
     description: z.string().min(1),
-    max_capacity: z.number().int().positive({message: "max_capacity debe ser mayor a cero "}),
+    max_capacity: z.number().int().positive({message: "max_capacity debe ser mayor a 0."}),
     additional_price: z.number().nonnegative(),
     requires_medical_certificate: z.boolean(),
 });
@@ -16,11 +16,17 @@ export class CreateSportUseCase {
     ) {}
 
     async execute(data: CreateSport): Promise<Sport> {
-        CreateSportSchema.parse(data);
+        try {
+            CreateSportSchema.parse(data);
+        } catch (error) {
+            if (error instanceof ZodError) {
+                throw new Error(error.issues[0].message);
+            }
+        }
 
         const deporteExistente = await this.sportRepository.findByName(data.name);
         if (deporteExistente && !deporteExistente.is_deleted) {
-            throw new Error(`El deporte con nombre "${data.name}" ya existe.`);
+            throw new Error(`No pueden existir dos instancias de Sport con el mismo nombre.`);
         }
 
         const nuevoDeporte = await this.sportRepository.create({
