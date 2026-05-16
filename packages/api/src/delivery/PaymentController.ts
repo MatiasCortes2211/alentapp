c216ae3f8a983e7d5a2a03164fd0a088cc1707b2
Angeles Schneeberger <angelesschneeberger@gmail.com>
2026-05-15 23:37:03 -0300

    feat(delivery): crea controlador para alta de pagos

---
+++ packages/api/src/delivery/PaymentController.ts
@@ -0,0 +1,42 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { CreatePaymentUseCase } from '../application/NewPaymentUseCase.js';
+import { CreatePaymentRequest } from '@alentapp/shared';
+
+export class PaymentController {
+    constructor(
+        private readonly createPaymentUseCase: CreatePaymentUseCase
+    ) {}
+
+    async create(
+        request: FastifyRequest<{ Body: CreatePaymentRequest }>,
+        reply: FastifyReply,
+    ) {
+        try {
+            request.log.info('Iniciando registro de nuevo pago');
+            
+            const pago = await this.createPaymentUseCase.execute(request.body);
+
+            return reply.status(201).send({ data: pago });
+            
+        } catch (error: any) {
+
+            if (error.message.includes('no existe en el sistema')) {
+                return reply.status(404).send({ error: error.message });
+            }
+
+            if (error.message.includes('Ya existe un pago activo')) {
+                return reply.status(409).send({ error: error.message });
+            }
+
+            if (
+                error.message.includes('mayor a cero') || 
+                error.message.includes('entre 1 y 12') || 
+                error.message.includes('no puede ser anterior a hoy')
+            ) {
+                return reply.status(400).send({ error: error.message });
+            }
+
+            return reply.status(500).send({ error: "Error interno, reintente más tarde" });
+        }
+    }
+}
