235e56c6eb083a5cfe72992347117bbe83394009
paulazf <paulavzf2004@gmail.com>
2026-05-17 19:51:03 +0000

    feat(api): agrega configuracion y endpoints para mostrar todas las disciplinas

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -36,6 +36,7 @@ import { PostgresDisciplineRepository } from './infrastructure/PostgresDisciplin
 import { DisciplineValidator } from './domain/services/DisciplineValidator.js';
 import { CreateDisciplineUseCase } from './application/NewDisciplineUseCase.js';
 import { DisciplineController } from './delivery/DisciplineController.js';
+import { GetDisciplineUseCase } from './application/GetDisciplineUseCase.js';
 
 export function buildApp() {
     const server = Fastify({
@@ -120,8 +121,11 @@ export function buildApp() {
 
     const createDisciplineUseCase = new CreateDisciplineUseCase(disciplineRepo, disciplineValidator, memberRepo);
 
+    const getDisciplineUseCase = new GetDisciplineUseCase(disciplineRepo);
+
     const disciplineController = new DisciplineController(
-        createDisciplineUseCase
+        createDisciplineUseCase,
+        getDisciplineUseCase
     );
 
     server.get('/api/v1/socios', memberController.getAll.bind(memberController));
@@ -142,6 +146,7 @@ export function buildApp() {
 
     //Endpoints para Discipline
     server.post('/api/v1/disciplines', disciplineController.create.bind(disciplineController));
+    server.get('/api/v1/disciplines', disciplineController.findAll.bind(disciplineController));
 
     server.get('/', async (req, rep) => {
         rep.status(200).send({ msg: 'asd' })
