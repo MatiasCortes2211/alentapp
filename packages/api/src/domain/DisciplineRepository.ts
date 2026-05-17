a156e2a4d11dba42cf8a1075604710fe80081868
paulazf <paulavzf2004@gmail.com>
2026-05-17 20:52:20 +0000

    feat(services): agrega metodo delete y findById a interfaz DisciplineRepository

--- packages/api/src/domain/DisciplineRepository.ts
+++ packages/api/src/domain/DisciplineRepository.ts
@@ -2,4 +2,8 @@ import { Discipline } from '@alentapp/shared';
 
 export interface DisciplineRepository {
   create(discipline: Omit<Discipline, 'id'>): Promise<Discipline>;
+
+  delete(id: string): Promise<void>;
+
+  findById(id: string): Promise<Discipline | null>;
 }
