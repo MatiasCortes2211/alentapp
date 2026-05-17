1e56d9279045ebc4ad870ba02dd53c09082af1e8
MatiasCortes2211 <mcortes@alu.frlp.utn.edu.ar>
2026-05-17 19:04:31 -0300

    feat(domain): agrega método delete a SportRepository

--- packages/api/src/domain/SportRepository.ts
+++ packages/api/src/domain/SportRepository.ts
@@ -4,4 +4,5 @@ export interface SportRepository {
     create(sport: Omit<Sport, 'id'>): Promise<Sport>;
     findByName(name: string): Promise<Sport | null>; // Agrega método para buscar por nombre. Permite evitar duplicados.
     findAll(): Promise<Sport[]>;
+    delete(id: string): Promise<void>;
 }
