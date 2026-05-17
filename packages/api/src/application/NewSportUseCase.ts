367227cb73610052dd886073a911744ed0cdac7d
MatiasCortes2211 <mcortes@alu.frlp.utn.edu.ar>
2026-05-16 21:16:43 -0300

    fix(zod): elimina CreateSportSchema de zod del Caso de Uso de Create Sport

--- packages/api/src/application/NewSportUseCase.ts
+++ packages/api/src/application/NewSportUseCase.ts
@@ -1,15 +1,8 @@
-import { z, ZodError } from "zod";
+import { ZodError } from "zod";
+import { CreateSportSchema } from "../domain/services/SportSchema.js";
 import { SportRepository } from "../domain/SportRepository.js";
 import { SportValidator } from "../domain/services/SportValidator.js";
 import {Sport, CreateSport } from "@alentapp/shared";
-
-const CreateSportSchema = z.object({
-    name: z.string().min(1, {message: "name es un campo requerido y no puede estar vacío."}),
-    description: z.string().min(1, {message: "description es un campo requerido y no puede estar vacío."}),
-    max_capacity: z.number().int().positive({message: "max_capacity debe ser mayor a 0."}),
-    additional_price: z.number().nonnegative({message: "additional_price debe ser mayor a 0."}),
-    requires_medical_certificate: z.boolean({message: "requires_medical_certificate debe ser un valor booleano válido."}),
-});
 
 export class CreateSportUseCase {
     constructor(
