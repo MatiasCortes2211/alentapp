ebc6d7d29b21c06b91d072c129a7a6213658deb0
Angeles Schneeberger <angelesschneeberger@gmail.com>
2026-05-17 22:05:36 -0300

    refactor(api): agrega validación en caso de uso de eliminación

--- packages/api/src/application/DeletePaymentUseCase.ts
+++ packages/api/src/application/DeletePaymentUseCase.ts
@@ -1,9 +1,18 @@
+import { z, ZodError } from 'zod';
 import { PaymentRepository } from '../domain/PaymentRepository.js';
 
 export class DeletePaymentUseCase {
     constructor(private readonly paymentRepository: PaymentRepository) {}
 
     async execute(id: string): Promise<void> {
+
+        try {
+            z.string().uuid({ message: "El formato del ID es inválido" }).parse(id);
+        } catch (error) {
+            if (error instanceof ZodError) throw new Error(error.issues[0].message);
+            throw error;
+        }
+        
         // 1. Verificar existencia del pago
         const existingPayment = await this.paymentRepository.findById(id);
         if (!existingPayment) {
