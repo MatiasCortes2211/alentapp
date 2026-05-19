1b303838d068ae6c951779d1d26fb6281d5194f7
MatiasCortes2211 <mcortes@alu.frlp.utn.edu.ar>
2026-05-19 17:38:23 -0300

    feat(schema): agrega validación con Zod para el Id y mantiene consistencia en los mensajes de error

--- packages/api/src/domain/services/SportSchema.ts
+++ packages/api/src/domain/services/SportSchema.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { id } from 'zod/locales';
 
 export const CreateSportSchema = z.object({
     name: z.string().min(1, {message: "El nombre no puede estar vacío."}),
@@ -9,6 +10,8 @@ export const CreateSportSchema = z.object({
 });
 
 export const UpdateSportSchema = z.object({
-    description: z.string().min(1, {message: "description no puede estar vacío."}).optional(),
-    max_capacity: z.number().int().positive({message: "max_capacity debe ser mayor a 0."}).optional(),
+    description: z.string().min(1, {message: "La descripción no puede estar vacía."}).optional(),
+    max_capacity: z.number().int().positive({message: "La capacidad máxima debe ser mayor a 0."}).optional(),
 });
+
+export const SportIdSchema = z.string().uuid({message: "ID inválido."});
