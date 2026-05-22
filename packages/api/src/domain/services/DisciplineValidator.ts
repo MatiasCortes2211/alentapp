297d7b7c8cb48e3e7c05b5bcd4001e73109ac24d
paulazf <paulavzf2004@gmail.com>
2026-05-22 02:16:56 +0000

    fix(services): elimina código de prueba por consola del DisciplineValidator

--- packages/api/src/domain/services/DisciplineValidator.ts
+++ packages/api/src/domain/services/DisciplineValidator.ts
@@ -1,7 +1,6 @@
 export class DisciplineValidator {
 
     async validateEndDate(start_date: string, end_date: string): Promise<void> {
-        console.log("-> Strings recibidos:", { start_date, end_date }); 
         
         const start = new Date(start_date);
         const end = new Date(end_date);
