89e1c2bf1d208346b407c1569093b48319884308
paulazf <paulavzf2004@gmail.com>
2026-05-24 01:39:19 +0000

    feat(services): agrega mensajes de campo obligatorio como validación Zod

--- packages/api/src/domain/services/DisciplineSchema.ts
+++ packages/api/src/domain/services/DisciplineSchema.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 const CreateDisciplineSchema = z.object({
-    reason: z.string().min(1, {message: "Razón es un campo requerido y no puede estar vacío."}).max(40, {message: "Razón no puede tener más de 40 caracteres."}),
-    start_date: z.string().min(1, {message: "La fecha de inicio es obligatoria"}),
-    end_date: z.string().min(1, {message: "La fecha de fin es obligatoria"}),
-    is_total_suspension: z.boolean({message: "Es suspensión Total debe ser un booleano"}),
-    member_id: z.string().uuid({message: "ID de miembro inválido."}),
+    reason: z.string({message: "Razón es un campo requerido y no puede estar vacío."}).min(1, {message: "Razón es un campo requerido y no puede estar vacío."}).max(40, {message: "Razón no puede tener más de 40 caracteres."}),
+    start_date: z.string({message: "La fecha de inicio es obligatoria"}).min(1, {message: "La fecha de inicio es obligatoria"}),
+    end_date: z.string({message: "La fecha de fin es obligatoria"}).min(1, {message: "La fecha de fin es obligatoria"}),
+    is_total_suspension: z.boolean({message: "Es suspensión Total es obligatorio y debe ser un booleano"}),
+    member_id: z.string({message: "ID de miembro es un campo obligatorio"}).uuid({message: "ID de miembro inválido."}),
 });
 
 const UpdateDisciplineSchema = z.object({
