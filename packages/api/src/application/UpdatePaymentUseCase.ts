048cb70ad04d20e874ed191aa9c70e49083c7b1c
Angeles Schneeberger <angelesschneeberger@gmail.com>
2026-05-24 20:27:51 -0300

    feat(application): agrega validacion para members suspendidos

--- packages/api/src/application/UpdatePaymentUseCase.ts
+++ packages/api/src/application/UpdatePaymentUseCase.ts
@@ -3,11 +3,13 @@ import { UpdatePaymentSchema, PaymentIdSchema } from '../domain/services/Payment
 import { PaymentRepository } from '../domain/PaymentRepository.js';
 import { PaymentValidator } from '../domain/services/PaymentValidator.js';
 import { PaymentDTO, PaymentStatus, UpdatePaymentRequest } from '@alentapp/shared';
+import { MemberRepository } from '../domain/MemberRepository.js';
 
 export class UpdatePaymentUseCase {
     constructor(
         private readonly paymentRepository: PaymentRepository,
-        private readonly paymentValidator: PaymentValidator
+        private readonly paymentValidator: PaymentValidator,
+        private readonly memberRepository: MemberRepository
     ) {}
 
     async execute(id: string, data: UpdatePaymentRequest): Promise<PaymentDTO> {
@@ -31,6 +33,9 @@ export class UpdatePaymentUseCase {
         if (!existingPayment) {
             throw new Error('El pago ingresado no existe en el sistema');
         }
+
+        const member = await this.memberRepository.findById(existingPayment.member_id);
+        this.paymentValidator.validateMemberIsNotSuspended(member.status);
 
         // 2. Verificar que no este ya eliminado
         if (existingPayment.is_deleted) {
