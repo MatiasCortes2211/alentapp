e6f6ab86962eba42563da8770b15ef918930b127
MatiasCortes2211 <mcortes@alu.frlp.utn.edu.ar>
2026-05-24 17:26:07 -0300

    refactor(application): integra validación de SpoirtIdSchema al caso de uso del Delete

--- packages/api/src/application/DeleteSportUseCase.ts
+++ packages/api/src/application/DeleteSportUseCase.ts
@@ -1,9 +1,20 @@
+import { ZodError } from 'zod';
 import { SportRepository } from "../domain/SportRepository.js";
+import { SportIdSchema } from '../domain/services/SportSchema.js';
 
 export class DeleteSportUseCase {
     constructor(private readonly sportRepo: SportRepository) {}
 
     async execute(id: string): Promise<void> {
+        try {
+            SportIdSchema.parse(id);
+        } catch (error) {
+            if (error instanceof ZodError) {
+                throw new Error(error.issues[0].message);
+            }   
+            throw error;
+        }
+
         const existingSport = await this.sportRepo.findById(id);
         if (!existingSport) {
             throw new Error('El deporte no existe');
