adaffe94f3035869f759a0d597a8be694647e0b3
paulazf <paulavzf2004@gmail.com>
2026-05-24 05:33:57 +0000

    feat(services): cambia mensajes de error de validación Zod para campos vacíos en Update de DisciplineSchema

--- packages/api/src/domain/services/DisciplineSchema.ts
+++ packages/api/src/domain/services/DisciplineSchema.ts
@@ -8,11 +8,11 @@ const CreateDisciplineSchema = z.object({
 });
 
 const UpdateDisciplineSchema = z.object({
-    reason: z.string().min(1, {message: "Razón es un campo requerido y no puede estar vacío."}).optional(),
-    start_date: z.string().min(1, {message: "La fecha de inicio es obligatoria"}).optional(),
-    end_date: z.string().min(1, {message: "La fecha de fin es obligatoria"}).optional(),
-    is_total_suspension: z.boolean({message: "Es suspensión Total debe ser un booleano"}).optional(),
-    member_id: z.string().uuid({message: "ID de miembro inválido"}).optional(),
+    reason: z.string().min(1, {message: "Razón no puede estar vacía."}).optional(),
+    start_date: z.string().min(1, {message: "La fecha de inicio no puede estar vacía"}).optional(),
+    end_date: z.string().min(1, {message: "La fecha de fin no puede estar vacía"}).optional(),
+    is_total_suspension: z.boolean({message: "Es suspensión Total no puede estar vacío y debe ser un booleano"}).optional(),
+    member_id: z.string().uuid({message: "ID de miembro vacío o inválido"}).optional(),
 });
 
 const DisciplineIdSchema = z.string().uuid({message: "ID de disciplina inválido"});
