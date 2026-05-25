68ba39501cdbdcc4b12e09b8a53ce4cecb78c1dd
paulazf <paulavzf2004@gmail.com>
2026-05-25 04:45:49 +0000

    feat(services): agrega validación de vigencia de la sanción

--- packages/api/src/domain/services/DisciplineValidator.ts
+++ packages/api/src/domain/services/DisciplineValidator.ts
@@ -1,6 +1,6 @@
 export class DisciplineValidator {
 
-    async validateEndDate(start_date: string, end_date: string): Promise<void> {
+    async validateEndDate(start_date: string, end_date: string): Promise<void> { //Valida que la fecha de fin sea mayor a la fecha de inicio
         
         const start = new Date(start_date);
         const end = new Date(end_date);
@@ -9,4 +9,15 @@ export class DisciplineValidator {
             throw new Error('La fecha de fin debe ser posterior a la fecha de inicio');
         }
     }
+
+    async validateDisciplineValidity(end_date: string): Promise<boolean> { //Valida que la fecha de fin no sea menor a la fecha actual
+        
+        const end = new Date(end_date);
+        const now = new Date(); 
+
+        if (end < now) {
+            return false;
+        }
+        return true;
+    }   
 }
