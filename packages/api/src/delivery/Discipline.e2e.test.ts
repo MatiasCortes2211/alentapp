f6dc246cd4de680f98942c2575931607c723dda3
paulazf <paulavzf2004@gmail.com>
2026-05-29 01:56:58 +0000

    test(delivery): agrega casos de creación de disciplina a API e2e test

--- packages/api/src/delivery/Discipline.e2e.test.ts
+++ packages/api/src/delivery/Discipline.e2e.test.ts
@@ -47,21 +47,82 @@ describe('Discipline API End-to-End Tests', () => {
             }
         });
         createdDisciplineId = discipline.id;
+
+
+
     });
 
     afterAll(async () => {
+        if (createdMemberId) {
+            await prisma.discipline.deleteMany({
+                where: { member_id: createdMemberId }
+            });
+        }
         if (createdMemberId) {
             await prisma.member.deleteMany({
                 where: { id: createdMemberId }
             });
         }
-        if (createdDisciplineId) {
-            await prisma.discipline.deleteMany({
-                where: { id: createdDisciplineId }
-            });
-        }
+
         await prisma.$disconnect();
         await app.close();
+    });
+
+        it('POST: Debe crear una disciplina con status 201 y mantener el estado del miembro si la disciplina no es vigente', async () => {
+            const today = new Date();
+            const pastDate1 = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate()).toISOString().split('T')[0];
+            const pastDate2 = new Date(today.getFullYear() - 2, today.getMonth(), today.getDate()).toISOString().split('T')[0];
+
+            const payload = {
+            reason: 'Motivo de la disciplina',
+            start_date: pastDate2,
+            end_date: pastDate1,
+            is_total_suspension: true,
+            member_id: createdMemberId,
+        };
+        const response = await app.inject({
+            method: 'POST',
+            url: '/api/v1/disciplines',
+            payload
+        });
+
+        expect(response.statusCode).toBe(201);
+        const body = JSON.parse(response.payload);
+        expect(body.data.is_deleted).toBe(false);
+        expect(body.data.member_id).toBe(createdMemberId);
+        const dbMember = await prisma.member.findUnique({
+            where: { id: createdMemberId }
+        });
+        expect(dbMember).not.toBeNull();
+        expect(dbMember?.status).toBe('Activo');
+    });
+
+    it('POST: Debe crear una disciplina con status 201 y estado de miembro a suspendido si la disciplina es vigente', async () => {
+        const today = new Date();
+        const futureDate1 = new Date(today.getFullYear() + 1, today.getMonth(), today.getDate()).toISOString().split('T')[0];
+        const futureDate2 = new Date(today.getFullYear() + 2, today.getMonth(), today.getDate()).toISOString().split('T')[0];
+
+        const payload = {
+            reason: 'Motivo de la disciplina',
+            start_date: futureDate1,
+            end_date: futureDate2,
+            is_total_suspension: true,
+            member_id: createdMemberId,
+        };
+        const response = await app.inject({
+            method: 'POST',
+            url: '/api/v1/disciplines',
+            payload
+        });
+        expect(response.statusCode).toBe(201);
+        const body = JSON.parse(response.payload);
+        expect(body.data.is_deleted).toBe(false);
+        expect(body.data.member_id).toBe(createdMemberId);
+        const dbMember = await prisma.member.findUnique({
+            where: { id: createdMemberId }
+        });
+        expect(dbMember).not.toBeNull();
+        expect(dbMember?.status).toBe('Suspendido');
     });
 
     it('DELETE: Debe eliminar la disciplina cambiando su estado is_deleted a true y devolver código 204', async () => {
